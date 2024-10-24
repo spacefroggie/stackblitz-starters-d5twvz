993b8d2a02844ded571a5f42b27297d21487789a
spacefroggie <marina.kalinichuk@gmail.com>
2024-10-24 10:31:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-d5twvz
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/spacefroggie/stackblitz-starters-d5twvz)
---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hello!');
+
+// Variables
+
+// Let variable saving the number 27 on the name age
+
+let age = 27
+console.log(age;)
+
+// Const variable saving the string Marina on the name name (what?)
+const name = "Marina"
